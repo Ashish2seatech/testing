c01b3df5832f40bc529e248d0a320559f3312958
Ashish2seatech <102578972+Ashish2seatech@users.noreply.github.com>
2024-12-07 23:15:46 +0530

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -5,7 +5,7 @@ pipeline {
         DOCKER_IMAGE = 'asisgolu95/demo-app'
         REMOTE_USER = "demo-user"            // SSH user for remote server
         SERVER_PRIVATE_IP = "13.232.205.26"       // Remote server IP or hostname
-        CREDENTIALSID = 'demo-user'  // Jenkins SSH credential ID
+        CREDENTIALSID = 'ssh-user-demo-user'  // Jenkins SSH credential ID
         DOCKER_COMPOSE_DIR = "/home/demo-user/" // Directory on remote server
     }
 
