11f3b06fda3a21ba67759f53a58ed9102630d42d
Ashish2seatech <102578972+Ashish2seatech@users.noreply.github.com>
2024-12-07 13:55:23 +0530

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -3,36 +3,43 @@ pipeline {
 
     environment {
         DOCKER_IMAGE = 'asisgolu95/demo-app'
-        DOCKER_REGISTRY = 'https://hub.docker.com/r/asisgolu95/demo-app'
-        
-        
+        DOCKER_REGISTRY = 'https://hub.docker.com/'
+        REGISTRY_CREDENTIAL = 'dockerhub_iD'
     }
 
     stages {
         stage('Checkout') {
             steps {
-                checkout scm
+                script {
+                    checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/Ashish2seatech/testing.git']]])
+                }
             }
         }
         stage('Build Docker Image') {
             steps {
                 script {
-                    sh 'sudo docker build -t $DOCKER_IMAGE .'
+                    sh 'docker build -t $DOCKER_IMAGE .'
                 }
             }
         }
         stage('Push to Docker Hub') {
             steps {
                 script {
-                    
-                    sh 'docker push $DOCKER_IMAGE'
+                    docker.withRegistry('$DOCKER_REGISTRY', '$REGISTRY_CREDENTIAL') {
+                        sh 'docker push $DOCKER_IMAGE'
+                    }
                 }
             }
         }
         stage('Deploy Docker Container') {
             steps {
                 script {
-                    sh 'docker run -d --name demo-app -p 3000:3000 --replicas 2 $DOCKER_IMAGE'
+                    // Stop and remove any existing container
+                    sh '''
+                    docker stop demo-app || true
+                    docker rm demo-app || true
+                    docker run -d --name demo-app -p 3000:3000 $DOCKER_IMAGE
+                    '''
                 }
             }
         }
@@ -45,10 +52,9 @@ pipeline {
                  body: "The Jenkins pipeline has completed successfully."
         }
         failure {
-            mail to: 'asisgolu@gmail.com',
+            mail to: 'asis.golu@gmail.com',
                  subject: "Jenkins Pipeline Failure",
                  body: "The Jenkins pipeline has failed. Please check the logs."
         }
     }
 }
-
